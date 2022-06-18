9273f92c7a271841e78d7ea37918a5d6272fd675
Alyssa Mannaioni <amannaioni210@gmail.com>
2022-06-17 22:00:21 -0500

    Category is editable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
-import {faHistory, faPenToSquare, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import { faHistory, faPenToSquare, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 import ScoreTable from './ScoreTable';
 import EditableScoreTable from './EditableScoreTable';
+import { Input } from '@mui/material';
+import { borderBottom } from '@mui/system';
+import { useState } from 'react';
+import MaterialEditableScoreTable from './MaterialEditableScoreTable';
 
 function App() {
+
   return (
     <div className="App">
       <div className='navBar'>
         <h1>FLAVALI</h1>
         <div className="headerIcons">
-        <FontAwesomeIcon icon={faUserGroup}/>
-        <FontAwesomeIcon icon={faPenToSquare}/>
-        <FontAwesomeIcon icon={faHistory}/> 
+          <FontAwesomeIcon icon={faUserGroup} />
+          <FontAwesomeIcon icon={faPenToSquare} />
+          <FontAwesomeIcon icon={faHistory} />
         </div>
       </div>
 
@@ -25,8 +30,13 @@ function App() {
 
       <h2>Game Name</h2>
 
+      <MaterialEditableScoreTable></MaterialEditableScoreTable>
+      <br></br>
+      {/*
       <ScoreTable></ScoreTable>
       <EditableScoreTable></EditableScoreTable>
+      */}
+      
     </div>
   );
 }
---
+++ src/MyScoreTable.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { createTable } from '@tanstack/react-table';
+
+const table = createTable()
+
+type  = {
+
+}
+
+
+
+function MyScoreTable() {
+    return (
+        <div></div>
+    );
+
+}
+
+export default MyScoreTable
