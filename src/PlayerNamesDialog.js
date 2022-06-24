9a9ab2d42a5497e5a49bfac12584080adc1a5ccb
Alyssa Mannaioni <amannaioni210@gmail.com>
2022-06-23 22:50:07 -0500

    add players text fields

--- src/PlayerNamesDialog.js
+++ src/PlayerNamesDialog.js
@@ -6,57 +6,76 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Grid from "@material-ui/core/Grid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+
 
 function PlayerNamesDialog() {
-  const [open, setOpen] = React.useState(false); 
+  const [isOpen, isSetOpen] = React.useState(false);
+  const [players, setPlayers] = React.useState(["Player 1", "Player 2"]);
 
   const handleClickOpen = () => {
-    setOpen(true);
+    isSetOpen(true);
   };
 
   const handleClose = () => {
-    setOpen(false);
+    isSetOpen(false);
   };
+
+  const renderPlayerTextFields = () => {
+    const playerTextFields = []
+    
+    players.forEach((player, index) => {
+      playerTextFields.push(
+        <TextField
+          key={player}
+          label={player}
+          autoFocus
+          margin="dense"
+          fullWidth
+          variant="standard"
+        />
+      );
+      console.log("player: ", player)
+      console.log("index: ", index)
+    })
+    return playerTextFields;
+  }
+
+  // ['Player 1', 'Player 2']
+
+  // ['Player 1', 'Player 2', 'Player 3'...]
+
+  const addPlayer = () => {
+    setPlayers()
+  }
+
+  
 
   return (
     <div>
       <Button>
         <FontAwesomeIcon icon={faUserGroup} onClick={handleClickOpen} />
       </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={isOpen} onClose={handleClose}>
         <DialogTitle>Player Names</DialogTitle>
         <DialogContent>
           <DialogContentText>
             Enter the players names and they will be added to the game cards.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Player 1"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Player 2"
-            fullWidth
-            variant="standard"
-          />
+          {renderPlayerTextFields()}
+          <Grid container justifyContent="center">
+            <Button>
+              <FontAwesomeIcon icon={faCirclePlus} onClick={addPlayer}/>
+            </Button>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={handleClose}>Enter</Button>
         </DialogActions>
       </Dialog>
-      {/* onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-      onMouseEnter={() => setMouse(true)}
-      onMouseLeave={() => setMouse(false)} */}
     </div>
   );
 }
