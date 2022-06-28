ac86c786c61614f5c52ce2a2dde141ef0b69335f
Luis Gomez <whoislewys@gmail.com>
2022-06-27 21:39:47 -0500

    [main] simplify default gamedata

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
 export const DEFAULT_GAME_DATA = {
-  players: [{ uid1: { name: "Player 1" } }, { uid2: { name: "Player 2" } }],
+  players: ["Player 1", "Player 2"],
   categories: [
     {
       name: "Chocolate",
       foodScores: [
         {
           foodName: "Item 1",
-          playerScores: [{ uid1: 0.0 }, { uid2: 0.0 }],
+          playerScores: [0.0, 0.0],
         },
         {
           foodName: "Item 2",
-          playerScores: [{ uid1: 0.0 }, { uid2: 0.0 }],
-        }
+          playerScores: [0.0, 0.0],
+        },
       ],
     },
     {
@@ -19,12 +19,12 @@ export const DEFAULT_GAME_DATA = {
       foodScores: [
         {
           foodName: "Item 1",
-          playerScores: [{ uid1: 0.0 }, { uid2: 0.0 }],
+          playerScores: [0.0, 0.0],
         },
         {
           foodName: "Item 2",
-          playerScores: [{ uid1: 0.0 }, { uid2: 0.0 }],
-        }
+          playerScores: [0.0, 0.0],
+        },
       ],
     },
   ],
