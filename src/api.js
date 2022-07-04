99dd02d5dd38e0c2a6e19215c1af41cce1aff8f2
Alyssa Mannaioni <amannaioni210@gmail.com>
2022-07-04 09:54:12 -0500

    updated game data to 5 default  cards

--- src/api.js
+++ src/api.js
@@ -7,17 +7,62 @@ export const DEFAULT_GAME_DATA = {
         {
           foodName: "Item 1",
           playerScores: [0.0, 0.0],
-          average: 7.0,
+          average: 0.0,
         },
         {
           foodName: "Item 2",
           playerScores: [0.0, 0.0],
-          average: 6.0,
+          average: 0.0,
         },
       ],
     },
     {
       name: "Sweet",
+      foodRows: [
+        {
+          foodName: "Item 1",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+        {
+          foodName: "Item 2",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+      ],
+    },
+    {
+      name: "Salty",
+      foodRows: [
+        {
+          foodName: "Item 1",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+        {
+          foodName: "Item 2",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+      ],
+    },
+    {
+      name: "Spicy",
+      foodRows: [
+        {
+          foodName: "Item 1",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+        {
+          foodName: "Item 2",
+          playerScores: [0.0, 0.0],
+          average: 0.0,
+        },
+      ],
+    },
+    {
+      name: "Wild",
       foodRows: [
         {
           foodName: "Item 1",
